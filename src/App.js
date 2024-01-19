1015f00dd7f823676739d862036af8f7f856643d
Sunz098 <119925492+Sunz098@users.noreply.github.com>
2024-01-19 20:49:11 +0700

    lastcommit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import Skills from "./components/Skill/skills";
 import Works from "./components/Work/works";
 import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer";
-
-
-
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Intro/>
-      <Skills/>
-      <Works/>
-      <Contact/>
-      <Footer/>
+      <Navbar />
+      <Intro />
+      <Skills />
+      <Works />
+      <Contact />
+      <Footer />
+      <div>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          ...
+        </Swiper>
+      </div>
     </div>
   );
 }
